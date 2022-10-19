9daefed2d40d85f8b54a3a4e7e6746ee3b7784a7
Your Name <koldaf20@gmail.com>
2022-10-18 23:11:40 -0700

    print alphabets in 10 places

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Print the alphabets in ten places
+* Return:0
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++
+}
+_putchar('\n');
+}
+return (0);
+}
+
