a5eab60bc2c16c8663558019acd43d70f4183985
Your Name <koldaf20@gmail.com>
2022-10-18 22:09:30 -0700

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - return putchar
  * Return: Zero
  */
 
-int main()
+int main(void)
 {
-wirte(1, "_putchar", 11);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
