21f1218d4f3cbf4517f3c38c097d1ee0d03cb43c
Your Name <koldaf20@gmail.com>
2022-10-18 22:23:06 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print alphabets
+ * Return: 0 always 
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while(c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+
