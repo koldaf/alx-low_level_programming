64e4653525e6df799665b3d89ce557f02bfb38fd
Your Name <koldaf20@gmail.com>
2022-10-20 10:55:29 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Another function to return
+ *Return: numbers from 0 - 9
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		printf(y);
+	}
+	return (0);
+}
