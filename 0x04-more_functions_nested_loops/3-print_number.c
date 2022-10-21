ec3cfeaef2f3f302814d6b6aed83c9c089fc9bdc
Your Name <koldaf20@gmail.com>
2022-10-20 22:25:50 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_number - print 0-9
+ *Return 0 - 9
+ */
+
+void print_number(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
