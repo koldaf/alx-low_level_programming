cfb70a970f6e9770fdc3040b0a27a8c766fcf713
Your Name <koldaf20@gmail.com>
2022-10-20 22:16:39 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul - multiplication
+ *@a: and integer to be declared
+ *@b: another integer
+ *Return: The value;
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
