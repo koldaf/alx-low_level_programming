c8ffe4a982a231a8d0523c1aef377788d6d2c893
Your Name <koldaf20@gmail.com>
2022-10-21 09:37:38 -0700

    print 1-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *more_numbers - Print 1-14 10 times
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if(j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+
+			_putchar(j % 10 + '0');
+
+		 }
+
+		_putchar('\n');
+	}
+
+
+}
