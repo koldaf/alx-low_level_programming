4592b027ec5d56a23408a41d0d55477ac05859e6
Your Name <koldaf20@gmail.com>
2022-10-21 11:59:25 -0700

    fix fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("%s ", "Fizz");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
 			printf("%s ", "Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("%s ", "FizzBuzz");
+		}
+		else if ( i == 1)
+		{
+			printf("%d ", i);
 		}
 		else
 		{
