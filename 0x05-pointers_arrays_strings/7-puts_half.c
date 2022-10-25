765fb827f52fa501b7788c2dc5c485b616827c5a
Your Name <koldaf20@gmail.com>
2022-10-24 23:25:04 -0700

    initial attempt

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *puts_half - print the second half
+ *@str: a string variabke
+ *Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{ /* get the total array length */
+	}
+
+	j = (i / 2);
+	if (j % 2 != 0)
+	{
+		_putchar(str[i]);
+	}
+	else
+	{
+		for (k = j + 1; k <= i; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+
+	_putchar('\n');
+}
