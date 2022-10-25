657975f9e3bff7be55703251367427fd7a3c470b
Your Name <koldaf20@gmail.com>
2022-10-24 18:55:26 -0700

    Print next

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other xter
+ *@str: string input
+ *Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 = 0)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
